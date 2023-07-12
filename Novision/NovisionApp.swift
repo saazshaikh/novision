fc750e336d69ca51a1a82d447aab068f0a7a176b
Saaz S <saazshaikh.edu@gmail.com>
2023-07-12 22:37:06 +0530

    Initial Commit

---
+++ Novision/NovisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovisionApp.swift
+//  Novision
+//
+//  Created by Saaz Shaikh on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NovisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
